435fee58348cde21e0e8b5d4bdf684840379024c
Justin Shreve <justin.shreve@gmail.com>
2017-06-19 15:32:33 -0700

    Add products UI state.

--- client/extensions/woocommerce/state/ui/products/reducer.js
+++ client/extensions/woocommerce/state/ui/products/reducer.js
@@ -5,12 +5,14 @@
 import { combineReducers, keyedReducer } from 'state/utils';
 import edits from './edits-reducer';
 import list from './list-reducer';
+import search from './search-reducer';
 import variations from './variations/reducer';
 import apiPlan from './api-plan/reducer';
 
 export default keyedReducer( 'siteId', combineReducers( {
 	list,
 	edits,
+	search,
 	variations,
 	apiPlan,
 } ) );
